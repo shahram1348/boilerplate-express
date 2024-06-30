226b1f8e0b33b71ef3fcab320643ca6f62100293
shahram1348 <shmotagh@gmail.com>
2024-06-30 20:26:47 +0330

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,11 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+// #1
+console.log("Hello World");
 
+// #2
+app.get("/", (req, res) => {
+  res.send("Hello Express1");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
